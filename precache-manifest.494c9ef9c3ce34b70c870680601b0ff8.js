b30c0e56f55d7654639efc1bc2ef955f9b9265da
Inbarasankumar <inbarasanraja@gmail.com>
2020-12-24 13:47:30 +0530

    Updates

---
+++ precache-manifest.494c9ef9c3ce34b70c870680601b0ff8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7435782d2dcae1544aeb7601d91738",
+    "url": "/ImageSearch_React/index.html"
+  },
+  {
+    "revision": "592830431395460f3040",
+    "url": "/ImageSearch_React/static/css/main.c94ea4b9.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/ImageSearch_React/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "592830431395460f3040",
+    "url": "/ImageSearch_React/static/js/main.320944c1.chunk.js"
+  },
+  {
+    "revision": "5d2087193901f4196a3e",
+    "url": "/ImageSearch_React/static/js/runtime~main.33290489.js"
+  }
+]);
